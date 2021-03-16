f928a7dc466e432ae86a979586420123470cf64b
GMkrtchyan <gmkrtchyan@alfabank.kz>
2021-03-17 01:30:28 +0300

    feat(*): add tests for string

---
+++ __tests__/validator.test.js
@@ -0,0 +1,33 @@
+import {
+  test, expect, describe,
+} from '@jest/globals';
+
+import Validator from '../src';
+
+describe('validator functionality', () => {
+  describe('string functionality', () => {
+    const v = new Validator();
+
+    const schema = v.string();
+
+    test('isValid', () => {
+      expect(schema.isValid('')).toBe(true);
+    });
+
+    test('required', () => {
+      expect(schema.required().isValid('')).toBe(false);
+    });
+
+    test('contains', () => {
+      expect(schema.contains('what').isValid('what does the fox say')).toBe(true);
+    });
+
+    test('minLength', () => {
+      expect(schema.minLength(10).isValid('ten')).toBe(false);
+    });
+
+    test('combination of methods', () => {
+      expect(schema.required().contains('what').minLength(10).isValid('what does the fox say')).toBe(true);
+    });
+  });
+});
